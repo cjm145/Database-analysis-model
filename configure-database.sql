3109b37b4d96c975599d4162f62b51d555d5e800
CodeJM <jiaming45@hotmail.com>
2023-06-06 12:10:47 +0800

    Delete configure-database.sql

--- configure-database.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS prof_data;
-CREATE DATABASE prof_data;
-
-USE prof_data;
-
-DROP USER IF EXISTS 'user' @'%';
-CREATE USER 'user' @'%' IDENTIFIED WITH mysql_native_password BY 'profession';
-GRANT ALL ON prof_data.* TO 'user' @'%';
